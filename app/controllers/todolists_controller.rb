218cc814fe942b067e4e637065537b73352161c4
mqcah <mqcha94@outlook.jp>
2020-06-08 22:03:54 +0900

    [finish]1-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -13,10 +13,21 @@ class TodolistsController < ApplicationController
 
         redirect_to '/top'
     end
+
+    def index
+        @lists = List.all
+    end
+
     private
+
+
    
     def list_params
         params.require(:list).permit(:title, :body)
     end
- 
+
+    
+
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
  
   post 'todolists' => 'todolists#create'
+
+
+ get 'todolists' => 'todolists#index'
+
 end
